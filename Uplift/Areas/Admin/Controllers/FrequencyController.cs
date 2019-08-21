b8efa970d51e1a2a1e960717dd599c3ab88f0a0a
nathanielpascual <nath_pascual@yahoo.com>
2019-08-21 23:58:09 +0800

    Added Upsert and modified frequency controller adding Upsert methods

--- Uplift/Areas/Admin/Controllers/FrequencyController.cs
+++ Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -20,13 +20,52 @@ namespace Uplift.Areas.Admin.Controllers
         {
             return View();
         }
+		[HttpGet]
+		public IActionResult Upsert(int? id)
+		{
+			Frequency frequency = new Frequency();
 
+			if (id == null) {
+				return View(frequency);
+			}
+
+			frequency = _unitOfWork.Frequency.Get(id.GetValueOrDefault());
+
+			if (frequency == null)
+			{
+				return NotFound();
+			}
+
+			return View(frequency);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Upsert(Frequency frequency) {
+			if (ModelState.IsValid) {
+				if (frequency.Id == 0)
+				{
+					_unitOfWork.Frequency.Add(frequency);
+				}
+				else {
+					_unitOfWork.Frequency.Update(frequency);
+				}
+
+				_unitOfWork.Save();
+
+				return RedirectToAction(nameof(Index));
+			}
+
+			return View(frequency);
+		}
 		#region API
 		[HttpGet]
 		public IActionResult GetAll()
 		{
 			return Json(new { data = _unitOfWork.Frequency.GetAll()});
 		}
+
+		
 		#endregion
 	}
 }
