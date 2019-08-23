15d02e68ef488dfde4200337c2d12d641d364a08
nathanielpascual <nath_pascual@yahoo.com>
2019-08-23 19:53:26 +0800

    Added ServiceController, index View, service.js and ServiceViewModel

---
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    public class ServiceController : Controller
+    {
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IWebHostEnvironment _hostEnvironment;
+		public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+		{
+			_unitOfWork = unitOfWork;
+			_hostEnvironment = hostEnvironment;
+		}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		#region API
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Json(new { data = _unitOfWork.Service.GetAll(includeProperties:"Category,Frequency") });
+		}
+
+		[HttpDelete]
+		public IActionResult Delete(int id)
+		{
+			var obj = _unitOfWork.Service.Get(id);
+
+			if (obj == null)
+			{
+				return Json(new { success= false, message="Error while deleting."});
+			}
+
+			_unitOfWork.Service.Remove(obj);
+			_unitOfWork.Save();
+
+			return Json(new { success = true, message = "Deleted Successful." });
+		}
+		#endregion
+	}
+}
---
+++ Uplift/wwwroot/js/service.js
@@ -0,0 +1,40 @@
+﻿var dataTable;
+$(document).ready(()=> {
+    loadDataTable();
+});
+
+loadDataTable = () => {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/service/GetAll",
+            "type": "GET",
+            "datatype" : "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "category.name", "width": "20%" },
+            { "data": "price", "width": "20%" },
+            { "data": "frequency.frequencyCount", "width": "20%" },  
+            {
+                "data": "id",
+                "render": (data) => {
+                    return `<div class="text-center">
+                                <a href="/Admin/service/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer;width:100px'>
+                                    <i class='far fa-edit'></i>&nbsp;Edit
+                                </a>
+                                 &nbsp;
+                                 <a onclick=Delete("/Admin/service/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer;width:100px'>
+                                    <i class='far fa-trash-alt'></i>&nbsp;Delete
+                                </a>
+                            </div>
+                    `
+                },"width":"20%"
+            }
+        ],
+        "language": {
+            "emptyTable" : "No records found"
+        },
+        "width":"100%"
+    });
+}
+
