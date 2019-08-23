9e9059fc86a57e39aecc7d23307bc23f8401c57f
nathanielpascual <nath_pascual@yahoo.com>
2019-08-23 17:56:52 +0800

    Added Service model to Models and to Database

---
+++ Uplift.Models/Service.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Uplift.Models
+{
+	public class Service
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name="Service Name")]
+		public string Name { get; set; }
+
+		[Required]
+		public double Price { get; set; }
+
+		[Display(Name ="Description")]
+		public string LongDescription { get; set; }
+
+		[DataType(DataType.ImageUrl)]
+		[Display(Name = "Image")]
+
+		public string ImageUrl { get; set; }
+
+		[Required]
+		public int CategoryId { get; set; }
+
+		[ForeignKey("CategoryId")]
+		public Category Category { get; set; }
+
+		[ForeignKey("FrequencyId")]
+		public Frequency Frequency { get; set; }
+	}
+}
