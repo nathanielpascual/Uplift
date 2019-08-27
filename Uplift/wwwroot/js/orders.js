cdd47f5aa44a28631951654b9736ed245986ff25
nathanielpascual <nath_pascual@yahoo.com>
2019-08-28 02:03:11 +0800

    Added orders.js and modified orderController with new api calls.

---
+++ Uplift/wwwroot/js/orders.js
@@ -0,0 +1,50 @@
+﻿var dataTable;
+
+$(document).ready(() => {
+    var url = window.location.search;
+
+    if (url.includes("approved")) {
+        loadDataTable("GetAllApprovedOrders");
+    }
+    else if (url.includes("pending"))  {
+        loadDataTable("GetAllPendingOrders");
+    }
+    else {
+        loadDataTable("GetAllOrders");
+    }
+});
+
+loadDataTable=(url)=> {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/order/" + url,
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "phone", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "serviceCount", "width": "15%" },
+            { "data": "status", "width": "15%" },
+            {
+                "data": "id",
+                "render":  (data)=> {
+                    return `<div class="text-center">
+                                <a href="/Admin/order/Details/${data}" class='btn btn-success text-white' style='cursor:pointer;width:100px'>
+                                    <i class='far fa-edit'></i>&nbsp;Details
+                                </a>
+                            </div>
+                           `;
+                },"width" : "20%"
+            }
+        ],
+        "language": {
+            "emptyTable" : "No records found"
+        },
+        "width" : "100%"
+    });
+}
+
+ 
+
