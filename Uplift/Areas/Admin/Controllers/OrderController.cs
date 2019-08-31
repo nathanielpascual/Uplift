6f176794593d4e00b3f810d76ff98e53d3db933f
nathanielpascual <nath_pascual@yahoo.com>
2019-08-31 11:02:12 +0800

    Added authorization in all Admin controllers

--- Uplift/Areas/Admin/Controllers/OrderController.cs
+++ Uplift/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Models.ViewModels;
@@ -9,6 +10,7 @@ using Uplift.Utility;
 
 namespace Uplift.Areas.Admin.Controllers
 {
+	[Authorize]
 	[Area("Admin")]
     public class OrderController : Controller
     {
--- Uplift/Areas/Admin/Controllers/UserController.cs
+++ Uplift/Areas/Admin/Controllers/UserController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Utility;
 
 namespace Uplift.Areas.Admin.Controllers
 {
+	[Authorize(Roles=StaticDetails.Admin)]
 	[Area("Admin")]
     public class UserController : Controller
     {
