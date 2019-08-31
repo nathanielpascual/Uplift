567f45737a68a623c6440f381fb564576fa3407e
nathanielpascual <nath_pascual@yahoo.com>
2019-08-31 22:21:29 +0800

    Added ISPRepository and USPRepository for stored procedure calls

---
+++ Uplift.DataAccess/Data/Repository/ISPRepository/ISP_Call.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.DataAccess.Data.Repository.ISPRepository
+{
+	public interface ISP_Call:IDisposable
+	{
+		IEnumerable<T> ReturnList<T>(string procedureName,DynamicParameters param=null);
+
+		T ExecuteScalar<T>(string procedureName, DynamicParameters param = null);
+
+		void ExecuteQuery(string procedureName, DynamicParameters param = null);
+
+	}
+}
---
+++ Uplift.DataAccess/Data/Repository/USPRepository/SP_Call.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Uplift.DataAccess.Data.Repository.ISPRepository;
+
+namespace Uplift.DataAccess.Data.Repository.USPRepository
+{
+	public class SP_Call : ISP_Call
+	{
+		private readonly ApplicationDbContext _db;
+		private static string ConnectionString = "";
+		public SP_Call(ApplicationDbContext db)
+		{
+			_db = db;
+			ConnectionString = _db.Database.GetDbConnection().ConnectionString;	
+		}
+
+		public void ExecuteQuery(string procedureName, DynamicParameters param = null)
+		{
+			using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+			{
+				sqlCon.Open();
+			    sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+
+			}
+		}
+
+		public T ExecuteScalar<T>(string procedureName, DynamicParameters param = null)
+		{
+			using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+			{
+				sqlCon.Open();
+				return (T)Convert.ChangeType(sqlCon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure),typeof(T));
+
+			}
+		}
+
+		public IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+		{
+			using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+			{
+				sqlCon.Open();
+				return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+
+			}
+		}
+
+		public void Dispose()
+		{
+			_db.Dispose();
+		}
+	}
+}
